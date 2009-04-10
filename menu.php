69cfc67766ae7b1d975953ce95475326cb83667f
nicolodavis <nicolodavis@d46fed13-6956-47a0-93a9-ae09a12bf88c>
2009-04-10 09:33:39 +0000

    Added admin control panel

--- menu.php
+++ menu.php
@@ -12,6 +12,7 @@
 		<?php if(isset($_SESSION['isloggedin'])) print '<li id="menu4"><a href="scores.php">Scoreboard</a></li>'; ?>
 		<li id="menu5"><a href="rules.php">FAQ</a></li>	
 		<?php if(isset($_SESSION['isloggedin'])) print '<li id="menu6"><a href="chat.php">Chat</a></li>'; ?>
-		<?php if(isset($_SESSION['isloggedin'])) print '<li id="menu7"><a href="logout.php">Logout</a></li>'; ?>
+		<?php if(isset($_SESSION['admin'])) print '<li id="menu7"><a href="admin.php">Admin</a></li>'; ?>
+		<?php if(isset($_SESSION['isloggedin'])) print '<li id="menu8"><a href="logout.php">Logout</a></li>'; ?>
 	</ul>
 </div>					
