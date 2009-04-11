7480ac2d06c6c70360f5a55538ad59de0ea5f1ee
nicolodavis <nicolodavis@d46fed13-6956-47a0-93a9-ae09a12bf88c>
2009-04-11 07:02:57 +0000

    Renamed rules.php to faq.php

--- menu.php
+++ menu.php
@@ -10,7 +10,7 @@
 		<?php if(isset($_SESSION['isloggedin'])) print '<li id="menu2"><a href="problem.php">Problems</a></li>'; ?>
 		<?php if(isset($_SESSION['isloggedin'])) print '<li id="menu3"><a href="submissions.php">Submissions</a></li>'; ?>
 		<?php if(isset($_SESSION['isloggedin'])) print '<li id="menu4"><a href="scores.php">Scoreboard</a></li>'; ?>
-		<li id="menu5"><a href="rules.php">FAQ</a></li>	
+		<li id="menu5"><a href="faq.php">FAQ</a></li>	
 		<?php if(isset($_SESSION['isloggedin'])) print '<li id="menu6"><a href="chat.php">Chat</a></li>'; ?>
 		<?php if(isset($_SESSION['admin'])) print '<li id="menu7"><a href="admin.php">Admin</a></li>'; ?>
 		<?php if(isset($_SESSION['isloggedin'])) print '<li id="menu8"><a href="logout.php">Logout</a></li>'; ?>
