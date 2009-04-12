c17f4b9580f795b0ecb2c795d48936b2fc1f1dcb
nicolodavis <nicolodavis@d46fed13-6956-47a0-93a9-ae09a12bf88c>
2009-04-12 06:22:39 +0000

    Updated footer

--- onj/footer.php
+++ onj/footer.php
@@ -5,5 +5,5 @@
 
 <p> 
 &copy;2008 <strong>Nicolo Davis and Sarang Bharadwaj</strong> <br/> 
-CSS by <a href="http://www.styleshout.com">styleshout</a> | Tabs by <a href="http://unraveled.com/publications/css_tabs/">CSS Tabs 2.0</a> | We use <a href="http://jquery.com">jQuery</a>
+CSS by <a href="http://www.styleshout.com">styleshout</a> | We use <a href="http://jquery.com">jQuery</a> | <a href="http://code.google.com/p/flot/">flot</a> | <a href="http://unraveled.com/publications/css_tabs/">CSS Tabs 2.0</a>
 </p>
