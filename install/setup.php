b3689f5b9eb2ac615e473c3cd4a637966237b50c
nicolodavis <nicolodavis@d46fed13-6956-47a0-93a9-ae09a12bf88c>
2009-09-05 01:44:35 +0000

    Added setup.php

---
+++ install/setup.php
@@ -0,0 +1,13 @@
+<?php
+
+	/*
+	* @copyright (c) 2008 Nicolo John Davis
+	* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+	*/
+
+	session_start();
+
+	include('../settings.php');
+
+	
+?>
