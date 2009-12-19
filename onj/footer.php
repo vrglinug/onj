4fa2f473525e4bd690e34ef89c28b3906e32ad4e
nicolodavis <nicolodavis@d46fed13-6956-47a0-93a9-ae09a12bf88c>
2009-12-19 02:09:52 +0000

    Extended year

--- onj/footer.php
+++ onj/footer.php
@@ -1,9 +1,9 @@
 <!--
-* @copyright (c) 2008 Nicolo John Davis
+* @copyright (c) 2008-2009 Nicolo John Davis
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
 -->
 
 <p> 
-&copy;2008 <strong>Nicolo Davis</strong> <br/> 
+&copy;2008-2009 <strong>Nicolo Davis</strong> <br/> 
 CSS by <a href="http://www.styleshout.com">styleshout</a> | ONJ uses <a href="http://jquery.com">jQuery</a> | <a href="http://code.google.com/p/flot/">flot</a> | <a href="http://unraveled.com/publications/css_tabs/">CSS Tabs 2.0</a>
 </p>
